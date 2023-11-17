39c17de20cdba8a85f1cdd578ac88049c30dff27
Roman <romantimilsina@gmail.com>
2023-11-17 11:58:23 +0545

    changes

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -2,7 +2,19 @@ import Gig from "../models/gig.model.js"
 import createError from "../utils/CreateError.js";
 
 export const createGig = async (req, res, next) => {
+  if(!req.isSeller) return next(createError(403, "Only sellers can create a gig!"))
 
+  const gig = new Gig({
+    userId: req.userId,
+    ...req.body,
+  })
+
+  try {
+    const savedGig = await gig.save();
+    res.status(201).json(savedGig)
+  }catch(err) {
+    next(err)
+  }
 }
 
 export const deleteGig = async (req, res, next) => {
