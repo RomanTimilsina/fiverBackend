832902e3b2e14b93a2ff75f11a2c67dec68ac4ab
Roman <romantimilsina@gmail.com>
2023-11-24 07:58:11 +0545

    GigCard

--- client/src/components/gigCard/GigCard.jsx
+++ client/src/components/gigCard/GigCard.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./GigCard.scss";
 import { Link } from "react-router-dom";
 
 const GigCard = ({ item }) => {
+  const [imageSrc, setImageSrc] = useState('');
+
+  console.log(item.cover)
+  useEffect(() => {
+    const downloadImage = () => {
+      fetch(`http://localhost:8800/api/uploads/${item.cover}`)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const imageUrl = URL.createObjectURL(blob);
+          setImageSrc(imageUrl);
+        })
+        .catch((error) => console.error('Error fetching image:', error));
+    };
+  
+    downloadImage();
+  }, [item]);
+
   return (
     <Link to="/gig/123" className="link">
       <div className="gigCard">
-        <img src={item.img} alt="" />
+        <img src={imageSrc} alt="" />
         <div className="info">
           <div className="user">
             <img src={item.pp} alt="" />
